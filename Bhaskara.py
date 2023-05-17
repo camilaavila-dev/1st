801550f945163d5cda7d0217006629a351116e9e
unknown <camilaavila.dev@gmail.com>
2023-05-16 21:06:46 -0300

    bhaskara

--- Bhaskara.py
+++ Bhaskara.py
@@ -1,4 +1,4 @@
-#Bhaskara formula
+#Bhaskara's formula
 
 import math
 
@@ -10,19 +10,21 @@ c = float(input("Enter the value of c: "))
 
 #Equation
 
-delta = ((b ** 2) - (4*a*c))
+delta = ((b ** 2) - (4 * a * c))
 
 #Results
 
 if delta < 0:
- print("This equation has no real roots")
+    print("This equation has no real roots")
 elif delta == 0:
- x0= -b / (2*a)
- print("The root of this equation is",x0)
+    x0 = -b / (2 * a)
+    print("The root of this equation is",x0)
 else:
- x1= (-b + math.sqrt(delta)) / (2*a)
- x2= (-b - math.sqrt(delta)) / (2*a)
- if x1 <= x2:
-  print("The roots of the equation are",x1,"e",x2)
- else:
-  print("The roots of the equation are",x2,"e",x1)
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    if x1 <= x2:
+        print(f"The roots of the equation are {x1} e {x2}.")
+        print(f"And delta is equal to {delta}.")
+    else:
+        print(f"The roots of the equation are {x2} e {x1}.")
+        print(f"And delta is equal to {delta}.")
